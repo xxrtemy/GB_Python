b3feca359b5382da6d25c9c181ab0569678545a9
xxrtemy <rebelgamechannel@gmail.com>
2023-03-03 13:46:56 +0300

    correct Sem4

--- Sem4/task24.py
+++ Sem4/task24.py
@@ -1,3 +1,15 @@
 
-# Не понял сути задания и сути решения 
-# Буду балгодарен, если объясните 
+import random
+
+count = int(input())
+list = []
+list_count = []
+
+for i in range(count):
+    list.append(random.randint(1,100))
+print(list)
+
+for i in range(len(list) - 1):
+    list_count.append(list[i-1] + list[i] + list[i+1])
+list_count.append(list[-2] + list[-1] + list[0])
+print(max(list_count))
